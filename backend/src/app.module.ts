499bb795c097ff099a4893cbd5a9b83abcce69f8
nchungojt07 <chung.nguyen-ojt07@devplus.edu.vn>
2024-03-04 14:58:47 +0700

    set up docker connect postgres

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { DbModule } from './common/db/db.module';
+import { UserModule } from './entities/users.entity';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }),
+  DbModule,
+  UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
