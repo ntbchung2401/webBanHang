ce272ee45b88a56ffe20758564be9c055e9d6feb
nchungojt07 <chung.nguyen-ojt07@devplus.edu.vn>
2024-03-06 15:37:07 +0700

    add sub folder for backend

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -5,21 +5,23 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
+  plugins: ['@typescript-eslint/eslint-plugin', 'prettier'],
+  extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
   root: true,
   env: {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js', 'sonar-scanner.ts'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { argsIgnorePattern: '^_', vars: 'all', args: 'after-used' },
+    ],
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
   },
 };
