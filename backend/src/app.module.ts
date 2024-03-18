2d06ab9f5d70c95c9db43ce1c6c3e9136e2e8e85
nchungojt07 <chung.nguyen-ojt07@devplus.edu.vn>
2024-03-19 03:57:06 +0700

    backend: crud category, brand success

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,19 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DbModule } from './common/db/db.module';
 import { UserModule } from './module/user/user.module';
+import { ProductModule } from './module/product/product.module';
+import { BrandModule } from './module/brand/brand.module';
+import { CategoryModule } from './module/category/category.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DbModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DbModule,
+    UserModule,
+    ProductModule,
+    BrandModule,
+    CategoryModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
